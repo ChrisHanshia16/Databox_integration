fc819537e4f34d015a25f5b450c053565e0b0d79
alnabeel-acs <111437266+alnabeel-acs@users.noreply.github.com>
2024-01-23 13:00:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -83,6 +83,3 @@ async def call_external_api(
     except httpx.HTTPError as e:
         raise HTTPException(status_code=500, detail=f"Error calling external API: {e}")
     
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
