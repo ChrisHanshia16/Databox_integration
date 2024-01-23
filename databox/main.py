dded7996391c549e88580f225e3e0ef39dabebc4
ajiths <109521950+ajithsacs@users.noreply.github.com>
2024-01-23 10:08:55 +0530

    bug fix

--- databox/main.py
+++ databox/main.py
@@ -5,13 +5,15 @@ import httpx
 app = FastAPI()
 
 
-@app.get("/v1/get")
-def get_hello():
-    return {"message": "Hello, this is a FastAPI GET API!"}
-
-
 @app.get("/v1/pipelines")
-async def call_external_api(pipeline: str = None, opportunities: str = None, source: str = None, name: str = None, status: str = None, monetaryValue: int = None):
+async def call_external_api(
+    pipeline: str = None,
+    opportunities: str = None,
+    source: str = None,
+    name: str = None,
+    status: str = None,
+    monetaryValue: int = None,
+):
     try:
         headers = {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IlhSMnFRc1JyWHAyMWIzQUdlcVZWIiwiY29tcGFueV9pZCI6IjZDa0tGR2d4Q0JwTUpXU082RlZWIiwidmVyc2lvbiI6MSwiaWF0IjoxNjk4MjI0MDc0NTY3LCJzdWIiOiJ1c2VyX2lkIn0.eKR11ubo3AoZxa2M9EamAfY6RCkwiIV9WsaiePFpiYg"
@@ -25,7 +27,18 @@ async def call_external_api(pipeline: str = None, opportunities: str = None, sou
 
             pipelines = response.json()["pipelines"]
 
-            pipeline_id = next((p["id"] for p in pipelines if p["name"] == pipeline), None)
+            pipeline_id = next(
+                (p["id"] for p in pipelines if p["name"] == pipeline), None
+            )
+            stage_id = ""
+            if opportunities:
+                for i in pipelines:
+                    if pipeline_id == i["id"]:
+                        for j in i["stages"]:
+                            if opportunities == j["name"]:
+                                stage_id = j["id"]
+                                break
+
             if pipeline_id is None:
                 raise HTTPException(status_code=404, detail="Pipeline not found")
 
@@ -36,17 +49,30 @@ async def call_external_api(pipeline: str = None, opportunities: str = None, sou
             opportunities_response.raise_for_status()
 
             opportunities_data = opportunities_response.json()["opportunities"]
-            
+
             if opportunities is not None:
-                filtered_opportunities = [op for op in opportunities_data if op["pipelineStageId"] == opportunities]
+                filtered_opportunities = [
+                    op for op in opportunities_data if op["pipelineStageId"] == stage_id
+                ]
+
             elif source is not None:
-                filtered_opportunities = [op for op in opportunities_data if op["source"] == source]
+                filtered_opportunities = [
+                    op for op in opportunities_data if op["source"] == source
+                ]
             elif name is not None:
-                filtered_opportunities = [op for op in opportunities_data if op["name"] == name]
+                filtered_opportunities = [
+                    op for op in opportunities_data if op["name"] == name
+                ]
             elif status is not None:
-                filtered_opportunities = [op for op in opportunities_data if op["status"] == status]
+                filtered_opportunities = [
+                    op for op in opportunities_data if op["status"] == status
+                ]
             elif monetaryValue is not None:
-                filtered_opportunities = [op for op in opportunities_data if op["monetaryValue"] == monetaryValue]
+                filtered_opportunities = [
+                    op
+                    for op in opportunities_data
+                    if op["monetaryValue"] == monetaryValue
+                ]
             else:
                 filtered_opportunities = opportunities_data
             if not filtered_opportunities:
